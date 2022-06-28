8043814b32a902c702dc932e1f84c804c0007b60
leongaban <leongaban@gmail.com>
2022-06-28 11:41:23 +0800

    removed cypress & fix build command

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,15 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
   ],
   rules: {
-    semi: [0, "error", "never"],
+    semi: ['error', 'never'],
   },
-  overrides: [
-    {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
-};
+  overrides: [],
+}

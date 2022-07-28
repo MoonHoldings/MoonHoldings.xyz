006b76ee0d10ec6a56f2f708cefef764be740cfb
leongaban <leongaban@gmail.com>
2022-07-28 11:13:55 +0800

    created coinStyles array

---
+++ src/constants/coinStyles.js
@@ -0,0 +1,18 @@
+[
+  {
+    id: "BTC",
+    name: "Bitcoin",
+    colors: {
+      text: "#000",
+      gradient: ["#F7931A", "#FFC783"]
+    }
+  },
+  {
+    id: "ALGO",
+    name: "Algorand",
+    colors: {
+      text: "#000",
+      background: "#fff"
+    }
+  }
+]

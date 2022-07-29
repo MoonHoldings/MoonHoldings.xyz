d2f60cbd14a7f76bad52b20a01c3fdf5bdbf63ca
leongaban <leongaban@gmail.com>
2022-07-29 17:59:55 +0800

    added coming soon page

---
+++ src/components/home/TempHomePage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="homepage">
+    <div class="parent">
+      <div class="child">
+        <img src="/svg/moon-holdings-logo.svg" width="420" alt="MoonHoldings.xyz" title="Logo art by Zen0"/>
+        <div class="copy">
+          <p>MoonHoldings.xyz</p>
+          <p class="small">is being built</p>
+          <a href="https://twitter.com/moonholdingsxyz" target="_blank">
+            <img src="twitter-logo.png" alt="MoonHoldings Twitter"/>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.parent {
+  height: 100%;
+  width: 100%;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+}
+
+.child {
+  margin-top: 50%;
+  height: 420px;
+  width: 420px;
+}
+
+.copy {
+  margin-top: 2rem;
+  text-align: center;
+
+  img {
+    margin-top: 2rem;
+  }
+}
+
+.small {
+  font-size: 1rem;
+}
+
+p {
+  text-align: center;
+  font-size: 2rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HomePage from '@/components/home/HomePage.vue'
+import TempHomePage from '@/components/home/TempHomePage.vue'
 </script>
 
 <template>
   <main>
-    <HomePage />
+    <!-- <HomePage /> -->
+    <TempHomePage />
   </main>
 </template>
 

f1c557c28c31b9a9299fa46cc35cd6c842ca1290
leongaban <leongaban@gmail.com>
2022-08-05 23:08:59 +0800

    cypress setup & first test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  projectId: "6n3mzk"
   // The rest of the Cypress config options go here...
+  projectId: "6n3mzk",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 }

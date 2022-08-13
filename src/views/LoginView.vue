79bfd1f43eed76483730d447fa044a03abe1e8d4
Abeer <abeer.technology@gmail.com>
2022-08-13 08:18:13 +0600

    password eye for login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,6 +17,7 @@ import { useUtilStore } from '@/stores/util'
 const clicks = ref(0)
 const email = ref('')
 const password = ref('')
+const isEyeOpen = ref('password')
 
 const errorEmail = ref(false)
 const errorPassword = ref(false)
@@ -83,6 +84,14 @@ const login = async () => {
     utilStore.mutate_errorMessage(error.message)
   }
 }
+
+const blink = () => {
+  if (isEyeOpen.value === 'password') {
+    isEyeOpen.value = 'text'
+  } else {
+    isEyeOpen.value = 'password'
+  }
+}
 </script>
 
 <template>
@@ -110,10 +119,52 @@ const login = async () => {
               class="p-box"
               :class="{ error: errorPassword, 'input-default': !errorPassword }"
               :style="{ transform: `translateX(${pTranslate}%)` }"
-              type="password"
+              :type="isEyeOpen"
               placeholder="Password"
               v-model="password"
             />
+            <transition
+              mode="out-in"
+              enter-active-class="animate__animated animate__fadeIn"
+              leave-active-class="animate__animated animate__fadeOut"
+            >
+              <button
+                v-if="pTranslate === 0"
+                @click.prevent="blink"
+                class="eye"
+              >
+                <svg
+                  v-if="isEyeOpen === 'text'"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 open"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <svg
+                  v-if="isEyeOpen === 'password'"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 closed"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                    clip-rule="evenodd"
+                  />
+                  <path
+                    d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"
+                  />
+                </svg>
+              </button>
+            </transition>
             <a href="/forgot-password">{{ FORGOT_PASSWORD }}</a>
             <button class="continue-btn" @click.prevent="continueBtn">
               {{ clicks === 1 ? SUBMIT : CONTINUE }}
@@ -169,6 +220,7 @@ const login = async () => {
 }
 
 .email-input {
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
@@ -184,6 +236,24 @@ const login = async () => {
     text-decoration: none;
     font-size: 1rem;
     margin-bottom: 20.41px;
+  }
+
+  .eye {
+    position: absolute;
+    top: 10px;
+    background: none;
+    border: none;
+    outline: none;
+    z-index: 25;
+
+    svg {
+      height: 30px;
+    }
+  }
+
+  .animate__animated.animate__fadeIn,
+  .animate__animated.animate__fadeOut {
+    --animate-duration: 1.5s;
   }
 }
 

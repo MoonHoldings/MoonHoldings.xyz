81e373b9a1a8072a3c0f498a8aaa3aa67434b945
Abeer <abeer.technology@gmail.com>
2022-09-01 10:10:44 +0600

    changed server to localhost

--- src/stores/coin.js
+++ src/stores/coin.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const localhost_api_url = 'http://localhost:9000/api'
 const prod_api_url = 'https://moonholdings-2.vercel.app/api'
 // const prod_api_url = 'https://moon-server.vercel.app/api'
-const api_url = prod_api_url // * Change this local / prod
+const api_url = localhost_api_url // * Change this local / prod
 
 export const useCoinStore = defineStore('coin', {
   state: () => ({

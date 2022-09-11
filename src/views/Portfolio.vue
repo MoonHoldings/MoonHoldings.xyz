d6f2bbe799bad6927d5b1f1f0c8bbea73c1dd855
Abeer <abeer.technology@gmail.com>
2022-09-11 15:16:45 +0600

    coin box design development

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -13,6 +13,7 @@ const coinStore = useCoinStore()
 const fetchedCoins = ref([])
 const coinNameInput = ref('')
 const searchedCoins = ref([])
+const showWelcome = ref(false)
 
 onMounted(async () => {
   //
@@ -45,23 +46,24 @@ const singleCoinSearch = async () => {
 <template>
   <Header />
   <div class="portfolio">
-    <div class="portfolio__coin-search">
-      <input v-model="coinNameInput" type="text" placeholder="Search Coins" />
-      <!-- <button @click="singleCoinSearch">Search</button> -->
-      <div class="sort-text">Portfolio display style:</div>
-      <div class="sort-btns">
-        <button class="grid-btn">
-          <img src="/svg/icon-grid.svg" alt="grid icon" />
-        </button>
-        <button class="list-btn">
-          <img src="/svg/icon-list.svg" alt="list icon" />
-        </button>
-      </div>
-      <!-- <transition
+    <div class="portfolio__entries">
+      <div class="portfolio__coin-search">
+        <input v-model="coinNameInput" type="text" placeholder="Search Coins" />
+        <!-- <button @click="singleCoinSearch">Search</button> -->
+        <div class="sort-text">Portfolio display style:</div>
+        <div class="sort-btns">
+          <button class="grid-btn">
+            <img src="/svg/icon-grid.svg" alt="grid icon" />
+          </button>
+          <button class="list-btn">
+            <img src="/svg/icon-list.svg" alt="list icon" />
+          </button>
+        </div>
+        <!-- <transition
         mode="out-in"
         enter-active-class="animate__animated animate__fadeInLeft"
         leave-active-class="animate__animated animate__fadeOutLeft"
-      >
+        >
         <div class="dropdown-list" v-if="searchedCoins.length !== 0">
           <ul>
             <li v-for="coin in searchedCoins" :key="coin.id">
@@ -69,9 +71,29 @@ const singleCoinSearch = async () => {
             </li>
           </ul>
         </div>
-      </transition> -->
+        </transition> -->
+      </div>
+      <!-- Coins -->
+      <div class="portfolio__coins">
+        <div v-for="(e, i) in 10" :key="i" class="portfolio__coin-box">
+          <div class="surface"></div>
+          <div class="shadow-common doge-shadow"></div>
+        </div>
+      </div>
     </div>
-    <div class="portfolio__welcome-msg">
+    <div class="portfolio__stats">
+      <div class="portfolio__total-value">
+        <div class="value">
+          <div class="value-number">
+            <span class="s1">$57,599.55</span> <span class="s2">+5.35%</span>
+            <small>24hr</small>
+          </div>
+        </div>
+        <div class="value-currency"></div>
+      </div>
+    </div>
+
+    <div v-if="showWelcome" class="portfolio__welcome-msg">
       <h1>{{ PORTFOLIO_WELCOME_HEADER }}</h1>
       <p>
         {{ PORTFOLIO_WELCOME_MSG1 }} <br />
@@ -85,18 +107,26 @@ const singleCoinSearch = async () => {
 </template>
 
 <style lang="scss" scoped>
+@import '@/sass/shadows.scss';
 .portfolio {
   padding: 1em;
-  display: inline-block;
+  display: grid;
+  grid-template-columns: 60% 40%;
   color: var(--ash);
   min-height: 100vh;
   min-width: 100%;
-  padding: 110px 26px 26px 26px;
+  padding: 110px 41px 41px 41px;
   background: linear-gradient(180deg, #f0f6f0 0%, #cecece 100%);
+
+  // ============== Entries ===============
+  &__entries {
+    padding-right: 30px;
+  }
 
   &__coin-search {
     display: flex;
     align-items: center;
+    margin-bottom: 58px;
 
     input {
       min-height: 44px;
@@ -151,6 +181,65 @@ const singleCoinSearch = async () => {
       }
     }
   }
+
+  &__coins {
+    display: flex;
+    gap: 18px;
+    flex-wrap: wrap;
+  }
+
+  &__coin-box {
+    position: relative;
+    .surface {
+      position: relative;
+      border: 1px solid #000;
+      border-radius: 2px;
+      background-color: #ffffff;
+      width: 180px;
+      height: 180px;
+      z-index: 6;
+    }
+  }
+
+  // ============ Stats =============
+  &__stats {
+  }
+
+  &__total-value {
+    .value {
+      background-color: #3f473f;
+      min-height: 64px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 600;
+      font-family: 'Inconsolata', monospace;
+
+      &-number {
+        position: relative;
+        .s1 {
+          font-size: 38px;
+          color: #fff;
+          margin-right: 15px;
+        }
+        .s2 {
+          font-size: 38px;
+          color: var(--green);
+        }
+        small {
+          // border: 1px solid magenta;
+          position: absolute;
+          bottom: 0;
+          margin-left: 15px;
+          font-size: 14px;
+          line-height: 42px;
+          color: #fff;
+        }
+      }
+    }
+  }
+
+  // ========== Welcome ===========
   &__welcome-msg {
     position: absolute;
     left: 50%;

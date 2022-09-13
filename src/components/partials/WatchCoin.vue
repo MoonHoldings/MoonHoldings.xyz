796d06213edcb8974a42b3e66686954099ffb2af
Abeer <abeer.technology@gmail.com>
2022-09-13 13:25:58 +0600

    coinbox and watchcoin separate components

---
+++ src/components/partials/WatchCoin.vue
@@ -0,0 +1,89 @@
+<script setup></script>
+
+<template>
+  <div class="coin-box">
+    <div class="surface">
+      <div class="symbol">
+        <div class="left">
+          <img
+            src="https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg"
+            alt=""
+          />
+          <span>BTC</span>
+        </div>
+        <div class="right">Bitcoin</div>
+      </div>
+      <div class="price">$23,955.69</div>
+      <div class="change">+2.69% 24 hr</div>
+    </div>
+    <div class="shadow-common doge-shadow"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/sass/shadows.scss';
+
+.coin-box {
+  position: relative;
+  .surface {
+    position: relative;
+    border: 1px solid #000;
+    border-radius: 2px;
+    background-color: #ffffff;
+    padding: 9px;
+    width: 180px;
+    height: 180px;
+    z-index: 6;
+
+    .symbol {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 25px;
+      .left {
+        display: flex;
+        align-items: center;
+
+        img {
+          width: 24px;
+          height: 24px;
+          margin-right: 7px;
+        }
+        span {
+          color: #000000;
+          font-weight: 700;
+          font-size: 14px;
+          font-family: 'Inter', monospace;
+        }
+      }
+      .right {
+        font-size: 14px;
+        color: #000000;
+        font-family: 'Inter', monospace;
+      }
+    }
+    .price,
+    .holdings,
+    .value {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .text,
+      .number {
+        font-size: 12px;
+      }
+    }
+    .price,
+    .holdings {
+      margin-bottom: 5px;
+    }
+    .value {
+      margin-bottom: 20px;
+    }
+    .change {
+      font-size: 12px;
+      text-align: center;
+    }
+  }
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue'
 import Header from '@/components/partials/Header.vue'
+import CoinBox from '@/components/partials/CoinBox.vue'
+import WatchCoin from '@/components/partials/WatchCoin.vue'
 import {
   PORTFOLIO_GET_STARTED,
   PORTFOLIO_WELCOME_HEADER,
@@ -14,15 +16,6 @@ const fetchedCoins = ref([])
 const coinNameInput = ref('')
 const searchedCoins = ref([])
 const showWelcome = ref(false)
-// const tempCoins = ref([
-//   {
-//     symbol: 'BTC',
-//     name: 'Bitcoin',
-//     price: '23,955.69',
-//     logo_url:
-//       'https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg',
-//   },
-// ])
 
 onMounted(async () => {
   //
@@ -84,67 +77,13 @@ const singleCoinSearch = async () => {
       </div>
       <!-- Coins -->
       <div class="portfolio__coins">
-        <div v-for="(e, i) in 7" :key="i" class="portfolio__coin-box">
-          <div class="surface">
-            <div class="symbol">
-              <div class="left">
-                <img
-                  src="https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg"
-                  alt=""
-                />
-                <span>BTC</span>
-              </div>
-              <div class="right">Bitcoin</div>
-            </div>
-            <div class="price">
-              <div class="text">Price</div>
-              <div class="number">$23,955.69</div>
-            </div>
-            <div class="holdings">
-              <div class="text">Holdings</div>
-              <div class="number">21</div>
-            </div>
-            <div class="value">
-              <div class="text">Value</div>
-              <div class="number">$692,069.49</div>
-            </div>
-            <div class="change">+2.69% 24 hr</div>
-          </div>
-          <div class="shadow-common doge-shadow"></div>
-        </div>
+        <CoinBox v-for="(e, i) in 7" :key="i" />
       </div>
       <!-- Watch List -->
       <div class="portfolio__watch">
         <h1>Watch</h1>
         <div class="portfolio__watch-coins">
-          <div v-for="(e, i) in 2" :key="i" class="portfolio__coin-box">
-            <div class="surface">
-              <div class="symbol">
-                <div class="left">
-                  <img
-                    src="https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg"
-                    alt=""
-                  />
-                  <span>BTC</span>
-                </div>
-                <div class="right">Bitcoin</div>
-              </div>
-              <div class="price">
-                <div class="text">Price</div>
-                <div class="number">$23,955.69</div>
-              </div>
-              <div class="holdings">
-                <div class="text">Holdings</div>
-                <div class="number">21</div>
-              </div>
-              <div class="value">
-                <div class="text">Value</div>
-                <div class="number">$692,069.49</div>
-              </div>
-              <div class="change">+2.69% 24 hr</div>
-            </div>
-            <div class="shadow-common doge-shadow"></div>
-          </div>
+          <WatchCoin v-for="(e, i) in 2" :key="i" />
         </div>
       </div>
     </div>
@@ -257,73 +196,10 @@ const singleCoinSearch = async () => {
     margin-bottom: 60px;
   }
 
-  &__coin-box {
-    position: relative;
-    .surface {
-      position: relative;
-      border: 1px solid #000;
-      border-radius: 2px;
-      background-color: #ffffff;
-      padding: 9px;
-      width: 180px;
-      height: 180px;
-      z-index: 6;
-
-      .symbol {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        margin-bottom: 25px;
-        .left {
-          display: flex;
-          align-items: center;
-
-          img {
-            width: 24px;
-            height: 24px;
-            margin-right: 7px;
-          }
-          span {
-            color: #000000;
-            font-weight: 700;
-            font-size: 14px;
-            font-family: 'Inter', monospace;
-          }
-        }
-        .right {
-          font-size: 14px;
-          color: #000000;
-          font-family: 'Inter', monospace;
-        }
-      }
-      .price,
-      .holdings,
-      .value {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        .text,
-        .number {
-          font-size: 12px;
-        }
-      }
-      .price,
-      .holdings {
-        margin-bottom: 5px;
-      }
-      .value {
-        margin-bottom: 20px;
-      }
-      .change {
-        font-size: 12px;
-        text-align: center;
-      }
-    }
-  }
-
   //-------------watch list--------------
   &__watch {
     h1 {
+      color: #000;
       font-size: 18px;
       font-family: 'Inter', monospace;
       margin-bottom: 20px;

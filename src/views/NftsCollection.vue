96323f20bf842cae136d1c68fcb273b8675a5ef3
Chris Sab <christopher.sab@outlook.com>
2022-10-25 02:20:50 +0900

    created nft card component

--- src/views/NftsCollection.vue
+++ src/views/NftsCollection.vue
@@ -10,6 +10,42 @@ import Header from '@/components/partials/Header.vue'
       <div class="label">
         Displaying 20 out of 64 collections, scroll for more
       </div>
+
+      <div class="grid">
+        <div class="grid-element">
+          <div class="grid-header">
+            <div class="header-left">
+              <div class="left-label">
+                Floor
+              </div>
+              <div class="left-number">
+                93.9
+              </div>
+            </div>
+            <div class="header-right">
+              <div class="image-container">
+                <img src="/svg/icon-arrow-circle-down.svg" alt="down value" />
+              </div>
+            </div>
+          </div>
+          <div class="grid-content"></div>
+          <div class="grid-name">
+            yoots: mint t00bs
+          </div>
+          <div class="grid-footer">
+            <div class="footer-info">
+              <div class="info-label">Items</div>
+              <div class="info-value">10</div>
+            </div>
+            <div class="footer-info">
+              <div class="info-label">Total</div>
+              <div class="info-label">939 = $29,109</div>
+            </div>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
     <div class="collection__right-side">
       <div class="label">

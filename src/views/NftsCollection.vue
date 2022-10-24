7daf8364ea9517f3d445a574904baffd68d0dfd4
Chris Sab <christopher.sab@outlook.com>
2022-10-25 02:35:38 +0900

    added shadow layer

--- src/views/NftsCollection.vue
+++ src/views/NftsCollection.vue
@@ -28,19 +28,24 @@ import Header from '@/components/partials/Header.vue'
               </div>
             </div>
           </div>
-          <div class="grid-content"></div>
-          <div class="grid-name">
-            yoots: mint t00bs
-          </div>
-          <div class="grid-footer">
-            <div class="footer-info">
-              <div class="info-label">Items</div>
-              <div class="info-value">10</div>
+          <div class="grid-box">
+            <div class="grid-container">
+              <div class="grid-content"></div>
+              <div class="grid-name">
+                yoots: mint t00bs
+              </div>
+              <div class="grid-footer">
+                <div class="footer-info">
+                  <div class="info-label">Items</div>
+                  <div class="info-value">10</div>
+                </div>
+                <div class="footer-info">
+                  <div class="info-label">Total</div>
+                  <div class="info-label">939 = $29,109</div>
+                </div>
+              </div>
             </div>
-            <div class="footer-info">
-              <div class="info-label">Total</div>
-              <div class="info-label">939 = $29,109</div>
-            </div>
+            <div class="grid-shadow"></div>
           </div>
         </div>
         <div></div>

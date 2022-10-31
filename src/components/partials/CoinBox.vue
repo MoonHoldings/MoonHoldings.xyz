fc3f08f2907172d65df8b1b744213f147395332e
Abeer <abeer.technology@gmail.com>
2022-10-31 21:08:01 +0600

    no logo on squares for which don't have logo url

--- src/components/partials/CoinBox.vue
+++ src/components/partials/CoinBox.vue
@@ -77,7 +77,7 @@ const clickCoinBox = () => {
     <div class="surface" @click="clickCoinBox">
       <div class="symbol">
         <div class="left">
-          <img :src="coin.logo_url" alt="" />
+          <img v-if="coin.logo_url" :src="coin.logo_url" alt="" />
           <span>{{ coin?.symbol }}</span>
         </div>
         <div class="right">{{ coin?.name }}</div>

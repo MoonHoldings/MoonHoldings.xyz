f89f9ce5a44d9dece285f83e7d541fbd1083ea53
Chris Sab <christopher.sab@outlook.com>
2022-11-01 16:46:01 +0900

    arranged codebase

--- src/stores/nft.js
+++ src/stores/nft.js
@@ -10,40 +10,32 @@ export const useNftStore = defineStore('nft', {
     shyft_url: `${import.meta.env.VITE_SHYFTSERVER_URL}`,
     shyft_key: `${import.meta.env.VITE_SHYFT_KEY}`,
     axios_config: { headers: { 'Content-Type': 'application/json' } },
-    nfts: []
+    portfolio: {}
   }),
   getters: {
-    get_nfts(state) {
-      return state.nfts
+    get_portfolio(state) {
+      return state.portfolio
     }
   },
   actions: {
-    mutate_emptyNfts() {
-      this.nfts = []
+    mutate_emptyPortfolio() {
+      this.portfolio = {}
     },
     async connectWalletWithAddress(walletAddress) {
-      const user = cookies.get('MOON_USER')
-      const token = cookies.get('MOON_TOKEN')
-      
       try {
-        console.log('2 connectWalletWithAddress', walletAddress)
         const response = await axios.get(
           `${this.shyft_url}/wallet/get_portfolio?network=mainnet-beta&wallet=${walletAddress}`,
           {
             headers: {
               'Content-Type': 'application/json',
-              // Authorization: token,
               'x-api-key': `${this.shyft_key}`
             }
           }
         )
 
-        console.log('3 response', response)
-
         const result = await response.data
-        console.log('result', result)
         if (result.success) {
-          console.log('get portfolio', result)
+          this.portfolio = result.result
         }
       } catch (error) {
         console.log('error', error)

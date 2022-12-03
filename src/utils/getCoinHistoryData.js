0f11deb84d2175a32274577ba97d912a4b5ea0ce
leongaban <leongaban@gmail.com>
2022-12-03 10:51:20 +0800

    getCoinHistory commented out for now from UI

--- src/utils/getCoinHistoryData.js
+++ src/utils/getCoinHistoryData.js
@@ -1,37 +1,37 @@
-import axios from 'axios'
-import { useCookies } from 'vue3-cookies'
+// import axios from 'axios'
+// import { useCookies } from 'vue3-cookies'
 
-import { SERVER_URL } from '@/constants/api'
+// import { SERVER_URL } from '@/constants/api'
 
-const { cookies } = useCookies()
+// const { cookies } = useCookies()
 
-export default async (historicalData, email) => {
-  try {
-    const token = cookies.get('MOON_TOKEN')
+// export default async (historicalData, email) => {
+//   try {
+//     const token = cookies.get('MOON_TOKEN')
 
-    const response = await axios.post(
-      `${SERVER_URL}/coin-history`,
-      {
-        historicalData,
-        email,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-        },
-      }
-    )
+//     const response = await axios.post(
+//       `${SERVER_URL}/coin-history`,
+//       {
+//         historicalData,
+//         email,
+//       },
+//       {
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Authorization: token,
+//         },
+//       }
+//     )
 
-    const result = await response.data
+//     const result = await response.data
 
-    if (result.success) {
-      return result.coinHistoryData
-    }
-  } catch (error) {
-    return {
-      success: false,
-      message: error.message,
-    }
-  }
-}
+//     if (result.success) {
+//       return result.coinHistoryData
+//     }
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error.message,
+//     }
+//   }
+// }

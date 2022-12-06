35a4899eae3ee2e3f17777ebdb9c9aa25f2dbae1
leongaban <leongaban@gmail.com>
2022-12-06 10:06:48 +0800

    truncaate functions created for NFT collection boxes

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -32,3 +32,11 @@ export const titleCreator = (name) => {
 
   return title
 }
+
+export const truncate = (str, n) => {
+  return (str.length > n) ? str.slice(0, n-1) + '...' : str;
+}
+
+export const truncateCollectionName = name => {
+  return truncate(name, 24)
+}
